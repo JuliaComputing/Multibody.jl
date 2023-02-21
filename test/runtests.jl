54a21d1f1c5d4b513f1bd1a30298d927b622bac0
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-02-21 09:14:58 +0100

    simplify initial test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,27 @@
 using ModelingToolkit
 using Multibody
-using ModelingToolkitStandardLibrary.Mechanical.TranslationalPosition
 t = Multibody.t
 
 world = Multibody.world
-@named spring = Spring(k=40)
 @named body = Body(; m=1)
 
 connections = [
-    connect(world.frame_b, spring.flange_a)
-    connect(spring.flange_a, body.frame_a)
+    connect(world.frame_b, body.frame_a)
+]
+
+@named model = ODESystem(connections, t, systems=[world, body])
+ssys = structural_simplify(model)
+
+
+## Add spring
+@named spring = Multibody.Spring(40)
+connections = [
+    connect(world.frame_b, spring.frame_a)
+    connect(spring.frame_a, body.frame_a)
 ]
 
 @named model = ODESystem(connections, t, systems=[world, spring, body])
+ssys = structural_simplify(model)
+
+
+
