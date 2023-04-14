7f996eb267368421d513dd1361572839260bf116
Joris Kraak <joris.kraak@juliahub.com>
2023-04-14 09:22:30 +0200

    Remove unnecessary `repo` keyword
    When using GitHub as the hosting platform, this keyword is unnecessary
    and in this case actually breaks Documenter's ability to determine the
    root URL of the repository[^1].
    
    The URL that was previously configured was also no longer correct anyway
    as the package has been moved to the JuliaComputing organization.
    
    [^1]: https://github.com/JuliaComputing/Multibody.jl/actions/runs/4687987328/jobs/8307961719#step:6:17

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ DocMeta.setdocmeta!(Multibody, :DocTestSetup, :(using Multibody); recursive = tr
 makedocs(;
          modules = [Multibody],
          authors = "Yingbo Ma <mayingbo5@gmail.com> and contributors",
-         repo = "https://github.com/YingboMa/Multibody.jl/blob/{commit}{path}#{line}",
          sitename = "Multibody.jl",
          format = Documenter.HTML(;
                                   prettyurls = get(ENV, "CI", nothing) == "true",
