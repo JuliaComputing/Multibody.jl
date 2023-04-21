bd3c417c3d21a5892ac95c4341c6a1a2cf5752b8
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-04-21 12:46:20 +0200

    add gearbox example

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ makedocs(;
              "Examples" => [
                  "Spring-damper system" => "examples/spring_damper_system.md",
                  "Spring-mass system" => "examples/spring_mass_system.md",
+                 "Gearbox" => "examples/gearbox.md",
              ],
          ])
 
