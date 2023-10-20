be39db2e6ef3d50a5b8557713fb5b19501d5f6af
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-10-20 15:43:49 +0200

    add spring, damper and spherical rendering

--- render/render.jl
+++ render/render.jl
@@ -1,4 +1,5 @@
 using GLMakie
+using LinearAlgebra
 
 """
     get_frame(sol, frame, t)
@@ -8,33 +9,41 @@ Extract a 4×4 transformation matrix ∈ SE(3) from a solution at time `t`.
 function get_frame(sol, frame, t)
     R = reshape(sol(t, idxs = vec(ori(frame).R.mat)), 3, 3)
     t = sol(t, idxs = collect(frame.r_0))
-    [R t; 0 0 0 1]
+    [R' t; 0 0 0 1]
 end
 
 
 """
-    render(model, sol; framerate = 30, timevec = range(sol.t[1], sol.t[end], step = 1 / framerate))
+    scene, time = render(model, sol, t::Real; framerate = 30)
+    path        = render(model, sol, timevec = range(sol.t[1], sol.t[end], step = 1 / framerate); framerate = 30)
 
 Create a 3D animation of a multibody system
 
 # Arguments:
 - `model`: The unsimplified multibody model
 - `sol`: The `ODESolution` produced by simulating the system using `solve`
+- `t`: If a single number `t` is provided, the mechanism at this time is rendered and a scene is returned together with the time as an `Observable`. Modify `time[] = new_time` to change the rendering.
+- `timevec`: If a vector of times is provided, an animation is created and the path to the file on disk is returned.
 - `framerate`: Number of frames per second.
-- `timevec`: The times at which to render a frame.
 """
-function render(model, sol;
+function render(model, sol,
+    timevec::Union{AbstractVector, Nothing} = nothing;
     framerate = 30,
-    timevec = range(sol.t[1], sol.t[end], step=1/framerate),
+    x = 0,
+    y = 0,
+    z = -10,
     )
+    if timevec === nothing
+        timevec = range(sol.t[1], sol.t[end], step=1/framerate)
+    end
     # with_theme(theme_dark()) do
         scene = Scene()
         
         cam3d!(scene)
         scene.camera.view[] = [
-            1 0 0 0
-            0 1 0 0
-            0 0 1 -10
+            1 0 0 x
+            0 1 0 y
+            0 0 1 z
             0 0 0 1
         ]
         t = 0.0
@@ -95,34 +104,37 @@ mesh!(scene, thing; style...)
 ```
 """
 function render!(scene, ::typeof(Body), sys, sol, t)
-    thing = @lift begin
+    thing = @lift begin # Sphere
         r = sol($t, idxs=collect(sys.r_cm))
-        # Ra, ta = get_frame(sol, sys.frame_a, $t)
-        # coords = ta + Ra * r
-
         Ta = get_frame(sol, sys.frame_a, $t)
-        coords = (Ta\[r; 1])[1:3] # TODO: make use of a proper transformation library instead of rolling own?
+        coords = (Ta*[r; 1])[1:3] # TODO: make use of a proper transformation library instead of rolling own?
         point = Point3f(coords)
         Sphere(point, 0.1)
     end
     mesh!(scene, thing, color=:purple)
-    thing = @lift begin
-        r = sol($t, idxs=collect(sys.r_cm))
-        # Ra, ta = get_frame(sol, sys.frame_a, $t)
-        # coords = ta + Ra * r
 
-        Ta = get_frame(sol, sys.frame_a, $t)
-        coords = (Ta\[r; 1])[1:3]
-        ta = Ta[1:3, 4]
+    # thing = @lift begin # Cylinder
+    #     Ta = get_frame(sol, sys.frame_a, $t)
+               
+    #     r = sol($t, idxs=collect(sys.r_cm))
+    #     coords = (Ta*[r; 1])[1:3]
+    #     point = Point3f(coords)
 
-        point = Point3f(coords)
+    #     rt = sol($t, idxs=collect(sys.r_0))
+    #     coords = (Ta*[rt; 1])[1:3]
+    #     tip = Point3f(coords)
 
-        origin = Point3f(ta)
-        extremity = point
-        radius = 0.02f0
-        Makie.GeometryBasics.Cylinder(origin, extremity, radius)
-    end
-    mesh!(scene, thing, color=:purple)
+    #     d = tip - point
+    #     d = d ./ norm(d)
+    #     # d = Point3f(Ta[1:3, 1])
+    #     tip = point + 0.2f0d
+    #     extremity = tip
+    #     origin = point
+
+    #     radius = 0.05f0
+    #     Makie.GeometryBasics.Cylinder(origin, extremity, radius)
+    # end
+    # mesh!(scene, thing, color=:purple)
 end
 
 function render!(scene, ::typeof(World), sys, sol, t)
@@ -150,6 +162,7 @@ function render!(scene, ::typeof(World), sys, sol, t)
 end
 
 function render!(scene, ::typeof(Revolute), sys, sol, t)
+    # TODO: change to cylinder
     thing = @lift begin
         vars = collect(sys.frame_a.r_0)
         coords = sol($t, idxs=vars)
@@ -159,14 +172,26 @@ function render!(scene, ::typeof(Revolute), sys, sol, t)
     mesh!(scene, thing, color=:red)
 end
 
+function render!(scene, ::typeof(Spherical), sys, sol, t)
+    thing = @lift begin
+        vars = collect(sys.frame_a.r_0)
+        coords = sol($t, idxs=vars)
+        point = Point3f(coords)
+        Sphere(point, 0.1)
+    end
+    mesh!(scene, thing, color=:yellow)
+end
+
+render!(scene, ::typeof(FreeMotion), sys, sol, t) = ()
+
 
 function render!(scene, ::typeof(FixedTranslation), sys, sol, t)
     thing = @lift begin
         r1 = Point3f(sol($t, idxs=collect(sys.frame_a.r_0)))
         r2 = Point3f(sol($t, idxs=collect(sys.frame_b.r_0)))
         origin = r1#(r1+r2) ./ 2
-        extremity = r2
-        radius = 0.1f0
+        extremity = r2#-r1 # Double pendulum is a good test for this
+        radius = 0.08f0
         Makie.GeometryBasics.Cylinder(origin, extremity, radius)
     end
     mesh!(scene, thing, color=:purple)
@@ -178,7 +203,7 @@ function render!(scene, ::typeof(BodyShape), sys, sol, t)
         r2 = Point3f(sol($t, idxs=collect(sys.frame_b.r_0)))
         origin = r1
         extremity = r2
-        radius = 0.08f0
+        radius = Float32(sol($t, idxs=sys.radius))
         Makie.GeometryBasics.Cylinder(origin, extremity, radius)
     end
     mesh!(scene, thing, color=:purple)
@@ -192,6 +217,31 @@ function render!(scene, ::typeof(BodyShape), sys, sol, t)
     # end
     # mesh!(scene, thing, color=:purple)
 end
+
+
+function render!(scene, ::typeof(Damper), sys, sol, t)
+    thing = @lift begin
+        r1 = Point3f(sol($t, idxs=collect(sys.frame_a.r_0)))
+        r2 = Point3f(sol($t, idxs=collect(sys.frame_b.r_0)))
+        origin = r1
+        d = r2 - r1
+        extremity = d / norm(d) * 0.2f0 + r1 
+        radius = 0.1f0
+        Makie.GeometryBasics.Cylinder(origin, extremity, radius)
+    end
+    mesh!(scene, thing, color=:gray)
+end
+
+
+function render!(scene, ::typeof(Spring), sys, sol, t)
+    thing = @lift begin
+        r1 = Point3f(sol($t, idxs=collect(sys.frame_a.r_0)))
+        r2 = Point3f(sol($t, idxs=collect(sys.frame_b.r_0)))
+        spring_mesh(r1,r2)
+    end
+    plot!(scene, thing, color=:blue)
+end
+
 
 "get_systemtype(sys): Get the constructor of a component for dispatch purposes. This only supports components that have the `gui_metadata` property set. If no metadata is available, nothing is returned."
 function get_systemtype(sys)
@@ -218,3 +268,42 @@ function render!(scene, ::Function, sys, sol, t, args...) # Fallback for systems
     end
 end
 
+
+
+
+function spring_mesh(p1, p2; n_wind=6, radius=0.1f0, N=200)
+    phi = range(0, n_wind*2π, length=N)
+    
+    d = p2 - p1
+    
+    x = radius*cos.(phi)
+    y = radius*sin.(phi)
+    z = range(0, norm(d), length=N) # Correct length
+    points = Point3f.(x, y, z)
+
+    d = d ./ norm(d)
+
+    # Rotate
+    R = rot_from_line(d)
+    points = Ref(R) .* points
+
+    # Translate
+    points = points .+ p1
+
+
+    Makie.GeometryBasics.LineString(points)
+end
+
+function rot_from_line(d)
+    d = d ./ norm(d)
+    if d[1] == 0 && d[2] == 0
+        return Matrix{Float32}(I, 3, 3)
+    end
+    d = d ./ norm(d)
+    z = [0, 0, 1]
+    x = cross(z, d)
+    x = x ./ norm(x)
+    y = cross(d, x)
+    y = y ./ norm(y)
+    RotMatrix{3}([x y d])
+end
