8daf349f91a126de6a9622ed55f7a05873a65fab
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-04-03 07:02:49 +0200

    more experiments

--- src/cb.jl
+++ src/cb.jl
@@ -47,7 +47,7 @@ function CraigBampton(; M, D=nothing, K, T, name)
     end
 
     node_transforms = map(1:n) do i
-        FixedRotation( # QUESTION: This prevents the nodes from moving w.r.t. each other, is this desired?
+        FixedRotation( # NOTE: This prevents the nodes from moving w.r.t. each other, the movement is handled by the interface nodes
             r = translations[i],
             n = rot_axes[i],
             angle = rot_angles[i];
@@ -57,9 +57,13 @@ function CraigBampton(; M, D=nothing, K, T, name)
 
     @named common_reference_frame = Frame()
 
+    # QUESTION: Should each mode also be associated with a Body attached to the interface node, + one body for the common frame? Can M be divided up into pieces for each body? If M is block-diagonal with at most 3x3 blocks, this would be easy.
+
     # Generalized reaction forces at interface nodes
-    forces = [[collect(node.frame_b.f); collect(node.frame_b.tau)] for node in interface_nodes]
-    forces = reduce(vcat, forces)
+    forces_b = [[collect(node.frame_b.f); collect(node.frame_b.tau)] for node in interface_nodes]
+    forces_a = [[collect(node.frame_a.f); collect(node.frame_a.tau)] for node in interface_nodes]
+    forces_b = reduce(vcat, forces_b)
+    forces_a = reduce(vcat, forces_a)
 
     @variables begin
         x(t)[1:ntot]=0, [description = "CB Displacement"]
@@ -68,15 +72,28 @@ function CraigBampton(; M, D=nothing, K, T, name)
         f(t)[1:ntot]=0, [description = "CB Reaction forces"]
     end
     x, v, a, f = collect.((x, v, a, f))
+    
+    
+    
+    #=
+    u = Φ*q # + H.O.T.
+    r = r_R + c + u
+
+    x = q in the modelica FlexibleBodies paper
+    there is a transformation Φ that relates elastic displacement u to the modal coordinates q according to u = Φq + H.O.T. This transformation depends on the point on the flexible object one is considering, i.e., Φ = Φ(c) ∈ R(3 × N)
+
+    Currently, we do not distinguish between u and q and have more or less u = q[1:3] = x[1:3]
+    =#
 
     if D === nothing
-        eqs = M*a       + K*x .~ f
+        eqs = M*a       + K*x .~ f # + gravity forces + quadratic velocity forces
     else
-        eqs = M*a + D*v + K*x .~ f
+        eqs = M*a + D*v + K*x .~ f # NOTE: see eq (7) in https:#www.bausch-gall.de/Session1d1.pdf This models the scenario when the motion of the reference frame is constrained to be zero
     end
     append!(eqs, Multibody.D.(x) .~ v)
     append!(eqs, Multibody.D.(v) .~ a)
-    append!(eqs, forces .~ f)
+    append!(eqs, forces_b .~ f)
+    # append!(eqs, forces_a .~ f) # The free motion is not completely free, it carries force through the interface
     for i = 1:n
         push!(eqs, connect(common_reference_frame, node_transforms[i].frame_a))
         push!(eqs, connect(node_transforms[i].frame_b, interface_nodes[i].frame_a))
@@ -99,6 +116,224 @@ end
 
 #=
 TODO
-- Handle the rigid body mode
+- Handle the rigid body mode and gravity
 - Add support for internal dynamic modes
 =#
+
+function FlexibleCoupling(; D=nothing, K, T, name)
+
+    ntot = LinearAlgebra.checksquare(K)
+    ntot == 6 || error("K must be a 6×6 matrix")
+    @parameters begin
+        K[1:ntot, 1:ntot] = K, [description = "CB Stiffness matrix"]
+    end
+    K = collect(K)
+
+    if D !== nothing
+        size(D) == (ntot, ntot) || error("D must be $ntot x $ntot")
+        @parameters begin
+            D[1:ntot, 1:ntot] = D, [description = "CB Damping matrix"]
+        end
+        D = collect(D)
+    end
+
+    translation = T2t(T)
+    rot_angles = rotation_angle(T2R(T))
+    @named fm = FreeMotion(r_rel_a = translation, phi = rot_angles, isroot=true)
+    @unpack frame_a, frame_b = fm
+    r_rel_a, phi = fm.r_rel_a, fm.phi
+    @variables begin
+        x(t)[1:ntot]=0, [state_priority = 10, description = "CB Displacement"]
+        v(t)[1:ntot]=0, [state_priority = 10, description = "CB Velocity"]
+        a(t)[1:ntot]=0, [state_priority = 10, description = "CB Acceleration"]
+        f(t)[1:ntot]=0, [state_priority = 10, description = "CB Reaction forces"]
+    end
+
+    @named begin
+        frame_a = Frame()
+        frame_b = Frame()
+    end
+
+    @named force = BasicForce(resolveInFrame = :frame_a)
+    @named torque = BasicTorque(resolveInFrame = :frame_a)
+
+    # Ra = ori(frame_a)
+
+    # fa = frame_a.f |> collect
+    # fb = frame_b.f |> collect
+    # taua = frame_a.tau |> collect
+    # taub = frame_b.tau |> collect
+    # r = translation + x[1:3]
+
+    # Rdeflect = axesRotations([1,2,3], x[4:6], D.(x[4:x]), name = :R_ar)
+
+    # eqs = [
+    #     D === nothing ? (K*x .~ f) : (D*v + K*x .~ f)
+    #     Multibody.D.(x) .~ v
+    #     Multibody.D.(v) .~ a
+    #     fa .+ fb .~ f[1:3]
+    #     taua .+ taub + cross(r, fb) .~ f[4:6]
+    #     frame_b.r_0 .~ frame_a.r_0 + resolve1(ori(frame_a), r)
+    #     ori(frame_b) ~ Rdeflect*ori(frame_a)
+    # ]
+
+    
+    eqs = [
+        (D === nothing ? (K*x) : (D*v + K*x)) .~ f
+        Multibody.D.(x) .~ v
+        Multibody.D.(v) .~ a
+
+        force.force.u .~ f[1:3]
+        torque.torque.u .~ f[4:6]
+        connect(frame_a, fm.frame_a, force.frame_a, torque.frame_a)
+        connect(frame_b, fm.frame_b, force.frame_b, torque.frame_b)
+
+        # frame_a.f + frame_b.f .~ force.force.u
+        # frame_a.tau + frame_b.tau .~ torque.torque.u
+        force.r_0 .~ r_rel_a
+        # torque.r_0 .~ r_rel_a
+
+        x[1:3] .~ r_rel_a .- translation
+        x[4:6] .~ phi .- rot_angles
+
+    ]
+
+    ODESystem(eqs, t; name, systems=[force, torque, frame_a, frame_b, fm])
+
+end
+
+
+function FlexibleLink(; b1, b2, D=nothing, K, T, name)
+
+    ntot = LinearAlgebra.checksquare(K)
+    ntot == 6 || error("K must be a 6×6 matrix")
+    @parameters begin
+        K[1:ntot, 1:ntot] = K, [description = "CB Stiffness matrix"]
+    end
+    K = collect(K)
+
+    if D !== nothing
+        size(D) == (ntot, ntot) || error("D must be $ntot x $ntot")
+        @parameters begin
+            D[1:ntot, 1:ntot] = D, [description = "CB Damping matrix"]
+        end
+        D = collect(D)
+    end
+
+    translations = map(T2t, T)
+    rot_axes = map(rotation_axis ∘ T2R, T)
+    rot_angles = map(rotation_angle ∘ T2R, T)
+
+    interface_nodes = map(1:n) do i
+        FreeMotion(
+            # r_rel_a = translations[i],
+            name = Symbol("interface$i"),
+            # isroot = false,
+        )
+    end
+
+    node_transforms = map(1:n) do i
+        FixedRotation( # NOTE: This prevents the nodes from moving w.r.t. each other, the movement is handled by the interface nodes
+            r = translations[i],
+            n = rot_axes[i],
+            angle = rot_angles[i];
+            name = Symbol("transform$i")
+        )
+    end
+
+    @named common_reference_frame = Frame()
+
+    # QUESTION: Should each mode also be associated with a Body attached to the interface node, + one body for the common frame? Can M be divided up into pieces for each body? If M is block-diagonal with at most 3x3 blocks, this would be easy.
+
+    # Generalized reaction forces at interface nodes
+    forces_b = [[collect(node.frame_b.f); collect(node.frame_b.tau)] for node in interface_nodes]
+    forces_a = [[collect(node.frame_a.f); collect(node.frame_a.tau)] for node in interface_nodes]
+    forces_b = reduce(vcat, forces_b)
+    forces_a = reduce(vcat, forces_a)
+
+    @variables begin
+        x(t)[1:ntot]=0, [description = "CB Displacement"]
+        v(t)[1:ntot]=0, [description = "CB Velocity"]
+        a(t)[1:ntot]=0, [description = "CB Acceleration"]
+        f(t)[1:ntot]=0, [description = "CB Reaction forces"]
+    end
+    x, v, a, f = collect.((x, v, a, f))
+    
+    
+    
+    #=
+    u = Φ*q # + H.O.T.
+    r = r_R + c + u
+    
+    x = q in the modelica FlexibleBodies paper
+    there is a transformation Φ that relates elastic displacement u to the modal coordinates q according to u = Φq + H.O.T. This transformation depends on the point on the flexible object one is considering, i.e., Φ = Φ(c) ∈ R(3 × N)
+
+    Currently, we do not distinguish between u and q and have more or less u = q[1:3] = x[1:3]
+    =#
+
+    if D === nothing
+        eqs =       K*x .~ f # + gravity forces + quadratic velocity forces
+    else
+        eqs = D*v + K*x .~ f # NOTE: see eq (7) in https:#www.bausch-gall.de/Session1d1.pdf This models the scenario when the motion of the reference frame is constrained to be zero
+    end
+    append!(eqs, Multibody.D.(x) .~ v)
+    append!(eqs, Multibody.D.(v) .~ a)
+    append!(eqs, forces_b .~ f)
+    # append!(eqs, forces_a .~ f) # The free motion is not completely free, it carries force through the interface
+    for i = 1:n
+        push!(eqs, connect(common_reference_frame, node_transforms[i].frame_a))
+        push!(eqs, connect(node_transforms[i].frame_b, interface_nodes[i].frame_a))
+    end
+
+
+    inds = 1:3
+    for i = 1:n
+        # assert equality between deflections and free motion coordinates
+        append!(eqs, collect(x[inds] .~ interface_nodes[i].r_rel_a))
+        inds = inds .+ 3
+        append!(eqs, collect(x[inds] .~ interface_nodes[i].phi))
+        inds = inds .+ 3
+    end
+
+    ODESystem(eqs, t; systems = [common_reference_frame; interface_nodes; node_transforms], name)
+
+end
+
+
+
+
+
+
+
+
+
+
+
+# # From https://www.tandfonline.com/doi/epdf/10.1080/13873954.2013.807433?needAccess=false
+# # Object-oriented modelling of general flexible multibody systemsGianni Ferrettia*, Alberto Levaaand Bruno Scaglioni
+
+# dq = der(q) # elastic coordinates velocities
+# ddq = der(dq) # elastic coordinates accelerations
+# [data.inv1*I(3),mD̄̃c',transpose(data.Ct)]*[aa_a - g_0;za_a;ddq] = h_w_r+h_e_r # (2)
+# [mD̄̃c,Jbar,Cr']*[aa_a - g_0;za_a;ddq]  =  h_w_θ+h_e_θ # (2)
+# [data.Ct,Cr,data.Me]*[aa_a - g_0;za_a;ddq] = h_w_f+h_e_f -(data.Ke*q)−((d/100)*(alpha*data.Me+beta*data.Ke)*dq) # (2)
+# mD̄c = data.inv2+sum(data.inv3[:,i]*q[i] for i in 1:M) # (4)
+# mD̄̃c = skew(mD̄c) # (4)
+# Cr' = data.inv4+sum(data.inv5[i,:,:]*q[i] for i in 1:M) # (6)
+# Jbar = data.inv7−sum((transpose(data.inv8[i,:,:])+data.inv8[i,:,:])*q[i] for i in 1:M)−sum(sum(data.inv9[i,j,:,:]*q[j] for j in i:M)*q[i] for i in 1:M) # (8)
+# h_w_r = (-cross(wa_a,cross(wa_a, mD̄c))−2*cross(wa_a, (transpose(data.Ct)*dq))) # (9)
+# h_w_θ = (-cross(wa_a, Jbar*wa_a) - (der(Jbar)*wa_a)−cross(wa_a, (Cr'*dq))) # (10)
+# h_w_f = (hwfint'*wa_a−2*[wa_a[1]*(data.inv11[3,2,:,:]-data.inv11[2,3,:,:])+wa_a[2]*(data.inv11[1,3,:,:]-data.inv11[3,1,:,:])+wa_a[3]*(data.inv11[2,1,:,:]-data.inv11[1,2,:,:])]*dq) # (12)
+# for i in 1:3
+#     for j in 1:3
+#         D[i,j,:] = data.inv10[i,j,:]+data.inv11[i,j,:,:]*q # (13)
+#     end 
+# end 
+# sumd = sum(D[i,i,:] for i in 1:3) # (13)
+# for i in 1:3
+#     hwfint[i,:] = wa_a[i]*sumd-wa_a*D[i,:,:] # (13)
+# end
+# h_e_r = (sum(fb_a[i,:] for i in 1:data.Nc)) # (14)
+# h_e_θ = (sum((taub_a[i,:]+cross((data.r0b[i,:]+data.Sb[i,:,:]*q),fb_a[i,:])) for i in 1:data.Nc)) # (15)
+# h_e_f = sum((transpose(data.Sb[i,:,:])*fb_a[i,:]+transpose(data.SbCap[i,:,:])*taub_a[i,:]) for i in 1:data.Nc) # (16)
+
--- test/test_cb.jl
+++ test/test_cb.jl
@@ -11,13 +11,83 @@ t = Multibody.t
 D = Differential(t)
 W(args...; kwargs...) = Multibody.world
 
+## Test with 1 DOF
+
+
+# M is generally Diagonal(mI, J)
+
+M = Diagonal([1, 1, 1, 1e-6, 1e-6, 1e-6])
+K = Diagonal([1, 1000, 1000, 1000, 1000, 1000])
+D = 0.0(M + K)
+
+
+T = [Rt2T(I(3), zeros(3)) for i = 1:n]
+@named cb = CraigBampton(; M, K, T)
+
+
+@mtkmodel CBTest begin
+    @components begin
+        world = W()
+        cb = CraigBampton(; M, K, T)
+        # body = Body(m=1)
+    end
+    @equations begin
+        connect(world.frame_b, cb.common_reference_frame)
+        # connect(cb.common_reference_frame, body.frame_a)
+    end
+end
+
+@named model = CBTest()
+model = complete(model)
+ssys = structural_simplify(IRSystem(model))
+
+
+
+x0 = [1; zeros(5)]
+prob = ODEProblem(ssys, [
+    collect(model.cb.x) .=> x0;
+    # model.world.g => 100;
+], (0.0, 20.0))
+sol = solve(prob, Rodas4())
+
+using Plots
+plot(sol)
+
+
+
+#
+plot(sol, idxs=collect(model.world.frame_b.f))
+plot(sol, idxs=collect(model.cb.f))
+plot(sol, idxs=collect(model.cb.interface1.frame_a.f))
+plot(sol, idxs=collect(model.cb.common_reference_frame.f))
+
+
+# After one period (2pi) we are back at the start
+@test sol(2pi, idxs=collect(model.cb.x)) ≈ x0 atol = 1e-3
+@test sol(2pi, idxs=collect(model.cb.v)) ≈ zeros(6) atol = 1e-3
+@test sol(0, idxs=collect(model.cb.interface1.frame_b.r_0)) ≈ T2t(T[1]) + x0[1:3] atol = 1e-3
+
+
+@test sol(pi/2, idxs=collect(model.cb.x[1:3])) ≈ sol(pi/2, idxs=collect(model.cb.interface1.frame_b.r_0)) atol = 1e-3 # The interface node coords are equal to x in this case when the node transformation is 0
+
+
+
+
+
+
+
+
+
+
+##
+
 Random.seed!(0)
 n = 1
 M = 1.0I(6*n)
 K = rand(6n, 6n)
 K = K'K
 
-D = 0.5M + 0.5K
+D = 0.05(M + K)
 
 
 if false
@@ -36,16 +106,9 @@ T = [Rt2T(I(3), rand(3)) for i = 1:n]
     @components begin
         world = W()
         cb = CraigBampton(; M, D, K, T)
-        # rev = Spherical()
-        # body = Body(m=1)
     end
     @equations begin
         connect(world.frame_b, cb.common_reference_frame)
-        # connect(world.frame_b, cb.interface1.frame_b)
-        # connect(cb.interface1.frame_b, body.frame_a)
-
-        # connect(world.frame_b, rev.frame_a)
-        # connect(rev.frame_b, cb.common_reference_frame)
     end
 end
 
@@ -66,6 +129,48 @@ plot(sol)
 
 plot(sol, idxs=collect(model.cb.f))
 
-
+@test sol(1, idxs=collect(model.cb.f)) != zeros(6n) 
 @test sol(prob.tspan[2], idxs=collect(model.cb.x)) ≈ zeros(6n) atol = 1e-6
 @test sol(prob.tspan[2], idxs=collect(model.cb.interface1.frame_b.r_0)) ≈ T2t(T[1])
+
+
+
+## test FlexibleCoupling
+
+M = Diagonal([1, 1, 1, 1e-6, 1e-6, 1e-6])
+K = Diagonal([1, 1, 1, 1, 1, 1])
+# D = 0.0(M + K)
+
+T = Rt2T(I(3), zeros(3))
+
+@mtkmodel TestFlex begin
+    @components begin
+        world = W()
+        fc = Multibody.FlexibleCoupling(; K, T)
+        body = Body(m=1)
+    end
+    @equations begin
+        connect(world.frame_b, fc.frame_a)
+        connect(fc.frame_b, body.frame_a)
+    end
+end
+
+@named model = TestFlex()
+model = complete(model)
+ssys = structural_simplify(IRSystem(model))
+prob = ODEProblem(ssys, [
+    # collect(model.fc.x) .=> randn(6);
+    # model.world.g => 100;
+], (0.0, 1.0))
+sol = solve(prob, Rodas4())
+plot(sol, idxs=[
+    collect(model.fc.f[2])
+    collect(model.fc.x[2])
+    collect(model.body.r_0[2])
+    # collect(model.fc.frame_a.r_0[2])
+    collect(model.fc.frame_b.r_0[2])
+
+], layout=1)
+
+# plot(sol, idxs=collect(model.fc.f))
+
