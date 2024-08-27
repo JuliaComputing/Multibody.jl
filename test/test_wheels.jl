2d3e5bf56cc32c1d77212ec7903aafbfee09bcdc
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-08-27 17:19:38 +0200

    add surface parameter to rolling wheel

--- test/test_wheels.jl
+++ test/test_wheels.jl
@@ -47,6 +47,74 @@ sol = solve(prob, Tsit5(), abstol=1e-8, reltol=1e-8)
 # ])
 
 
+# ==============================================================================
+## Rolling wheel on interesting surface ===============================================================
+# ==============================================================================
+using LinearAlgebra
+# The wheel does not need the world
+# @testset "Rolling wheel" begin
+@mtkmodel WheelInWorld begin
+    @structural_parameters begin
+        surface
+    end
+    @components begin
+        # world = World(n=[0,0,-1])
+        world = W()
+        wheel = RollingWheel(; radius = 0.3, m = 2, I_axis = 0.06,
+                            I_long = 0.12,
+                            x0 = 0.2,
+                            z0 = 0.2,
+                            surface,
+                            der_angles = [0, 0, 0])
+    end
+end
+
+@named worldwheel = WheelInWorld(surface = (x,z)->0)
+worldwheel = complete(worldwheel)
+
+# pars = collect(worldwheel.world.n) .=> [0,0,-1];
+defs = Dict([
+    # collect(worldwheel.world.n) .=> [0,0,-1];
+    worldwheel.wheel.body.r_0[1] => 0.2;
+    worldwheel.wheel.body.r_0[2] => 0.3;
+    worldwheel.wheel.body.r_0[3] => 0.2;
+    collect(worldwheel.wheel.rollingWheel.der_angles) .=> [0, 5, 1];
+    # collect(D.(cwheel.rollingWheel.angles)) .=> [0, 5, 1]
+])
+
+ssys = structural_simplify(IRSystem(worldwheel))
+prob = ODEProblem(ssys, defs, (0, 4))
+sol = solve(prob, FBDF(autodiff=false), abstol=1e-8, reltol=1e-8)
+@test SciMLBase.successful_retcode(sol)
+# first(Multibody.render(worldwheel, sol, 0, show_axis=true))
+@test sol(4, idxs=[worldwheel.wheel.x; worldwheel.wheel.z]) ≈ [0.162547, -2.23778] atol=1e-3
+
+@named worldwheel = WheelInWorld(surface = (x,z)->x)
+worldwheel = complete(worldwheel)
+
+defs = Dict([
+    # collect(worldwheel.world.n) .=> [0,0,-1];
+    worldwheel.wheel.body.r_0[1] => 0.0;
+    worldwheel.wheel.body.r_0[2] => 0.3/sqrt(2);
+    worldwheel.wheel.body.r_0[3] => 0.0;
+    collect(worldwheel.wheel.rollingWheel.der_angles) .=> [0, 0, 0];
+])
+
+ssys = structural_simplify(IRSystem(worldwheel))
+prob = ODEProblem(ssys, defs, (0, 4))
+sol = solve(prob, FBDF(autodiff=false), abstol=1e-8, reltol=1e-8)
+# plot(sol)
+tv = 0:0.5:4
+@test sol(tv, idxs=worldwheel.wheel.body.r_0[1]) ≈ sol(tv, idxs=worldwheel.wheel.body.r_0[2]) .- 0.3*sqrt(2) rtol=1e-6 # The sqrt(2) is to account for the shifted contact point at a 45 degree plane
+
+dd = diff(sol(tv, idxs=worldwheel.wheel.rollingWheel.der_angles[2]).u) # angular acceleration
+@test norm(dd .- dd[1]) < 1e-10 # constant acceleration
+@test abs(dd[1]) < 9.81
+@test abs(dd[1]) > 5
+
+
+
+
 ## Rolling wheel with axis ====================================================
 import ModelingToolkitStandardLibrary.Blocks
 @mtkmodel WheelWithAxis begin
